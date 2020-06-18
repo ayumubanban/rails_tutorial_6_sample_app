7a41232ab35ce43cf9a51ec58bf7defdaaafbc98
ayumubanban <ayumuban@outlook.com>
2020-06-18 21:10:54 +0900

    :sparkles: Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,7 +10,15 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
   include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+
+  # ヘルパーメソッドはテストから呼び出せない。よって、current_userを呼び出せない。
+  # sessionメソッドはテストでも利用できる
+  # logged_in?の代わりに定義
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
