5ebaab79a7dd803653cd0b2d42da36fe0a277e99
ayumubanban <ayumuban@outlook.com>
2020-06-21 21:14:36 +0900

    :sparkles: Solve rest practices

--- app/models/user.rb
+++ app/models/user.rb
@@ -112,6 +112,9 @@ class User < ApplicationRecord
     # このサブセレクトは集合のロジックを（Railsではなく）データベース内に保存するので、より効率的にデータを取得することができる
     following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
     Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+    # 演習 リスト14.50
+    # part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    # Micropost.joins(user: :followers).where(part_of_feed, { id: id })
   end
 
   # ユーザーをフォローする
